45d7f5da8fac6678bfdf21caae1a6bc6930e3a59
sbalexandru <sbalexandru@yahoo.com>
2021-12-30 11:05:28 +0200

    file creation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainPage from "./components/MainPage";
+
 function App() {
   return (
-    <div>
-      <p>ani text</p>
-    </div>
+    <>
+      <Header />
+      <MainPage />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>ceva head</p>
+    </div>
+  );
+};
+
+export default Footer;
